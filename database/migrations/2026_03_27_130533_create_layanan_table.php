414633fc26979b014d827623afbf522f7b8b5d6a
p3kzz <bolangspeed09@gmail.com>
2026-03-27 20:06:40 +0700

    feat: create table layanan

---
+++ database/migrations/2026_03_27_130533_create_layanan_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('layanan', function (Blueprint $table) {
+            $table->id();
+             $table->foreignId('kategori_id')->constrained('kategori_layanan')->cascadeOnDelete();
+
+            $table->string('kode')->nullable();
+            $table->string('nama');
+
+            $table->text('deskripsi')->nullable();
+            $table->string('pengguna_sasaran')->nullable();
+            $table->string('service_owner')->nullable();
+            $table->string('jam_layanan')->nullable();
+
+            $table->text('sla')->nullable();
+            $table->text('biaya')->nullable();
+            $table->text('cara_akses')->nullable();
+
+            $table->string('status')->default('Aktif');
+            $table->string('dependencies')->nullable();
+            $table->string('kontak')->nullable();
+
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('layanan');
+    }
+};
